8c51d857cbe12a01de382c46f2e7b086cbb745e4
Sachin Kumar <sachinkumarskpog@gmail.com>
2026-02-28 19:43:43 +0530

    added axios interceptors

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,10 +2,21 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-console.log("PROD API:", import.meta.env.VITE_API_BASE_URL);
-console.log("PROD API 2:", API_BASE_URL);
-
 export const axiosInstance = axios.create({
     baseURL: `${API_BASE_URL}/api`,
     withCredentials: true,
 });
+
+// 🔥 Add this interceptor
+axiosInstance.interceptors.request.use(
+    async (config) => {
+        if (window.Clerk?.session) {
+            const token = await window.Clerk.session.getToken();
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+        }
+        return config;
+    },
+    (error) => Promise.reject(error),
+);
